19b86458d3088c8e056516d42d953355063b7cb3
Gaurav <gaurav360@outlook.com>
2019-02-03 15:16:23 +0530

    updated

--- SampleAPIService/SampleAPIService/Controllers/ValuesController.cs
+++ SampleAPIService/SampleAPIService/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace SampleAPIService.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "Monika", "Sujit" };
         }
 
         // GET api/values/5
